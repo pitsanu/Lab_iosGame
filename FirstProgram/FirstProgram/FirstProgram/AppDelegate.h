c26dd17897db7837dfafccb3765b1047b7a24aba
pitsanu <p.tongprasin@gmail.com>
2013-12-01 09:26:18 +0700

    Add new project

---
+++ FirstProgram/FirstProgram/FirstProgram/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FirstProgram
+//
+//  Created by Tom on 12/1/13.
+//  Copyright (c) 2013 The Big Mud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FirstProgram/FirstProgram/FirstProgram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstProgram
+//
+//  Created by Tom on 12/1/13.
+//  Copyright (c) 2013 The Big Mud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
