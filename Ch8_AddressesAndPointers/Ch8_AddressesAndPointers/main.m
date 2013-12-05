1e01a92014c73e4bd1788960b589e50e75387167
pitsanu <p.tongprasin@gmail.com>
2013-12-05 09:11:17 +0700

    Add chapter 8 Addresses and Pointers

---
+++ Ch8_AddressesAndPointers/Ch8_AddressesAndPointers/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ch8_AddressesAndPointers
+//
+//  Created by Tom on 12/5/13.
+//  Copyright (c) 2013 The Big Mud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
