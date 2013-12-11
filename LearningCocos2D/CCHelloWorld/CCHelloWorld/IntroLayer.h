1e8435649cbe3d12aab459f9127f713885a2d0ea
pitsanu <p.tongprasin@gmail.com>
2013-12-11 21:41:42 +0700

    Complete RandomPossesions chapter 2

---
+++ LearningCocos2D/CCHelloWorld/CCHelloWorld/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  CCHelloWorld
+//
+//  Created by Tom on 12/10/13.
+//  Copyright The Big Mud 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
